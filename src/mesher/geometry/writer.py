206d044f8c6dd5f8acde4ca5a8e92e22f88f0bf3
Jeremy Montera <wittkopp.jeremy@gmail.com>
2024-01-03 21:17:11 -0500

    Mesh8.5.2 - implemented writer

---
+++ src/mesher/geometry/writer.py
@@ -0,0 +1,29 @@
+import os
+import pathlib
+
+from .abc import IRing, IWriter
+
+class Writer(IWriter):
+    
+    @staticmethod
+    def write(filename: str | pathlib.Path, data: dict[str, IRing]) -> None:
+        """
+        Write point data from figures to file.
+
+        TODO: update this when polygons/cells are implemented.
+
+        Args:
+            filename:
+                File to write geometric data to.
+            data:
+                Dictionary of ring names and rings.
+        """
+
+        if os.path.exists(filename):
+            raise OSError(f"{filename} already exists!")
+
+        with open(filename, "w") as f:
+            for name, ring in data.items():
+                f.write(f"{name}\n")
+                for point in ring:
+                    f.write(f"{point.x},{point.y},{point.ID}\n")
